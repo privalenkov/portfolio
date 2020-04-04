c66e74312620d7a0c3cb29625c03062c5f838dbe
privalenkov <privalenkovkirill@gmail.com>
2020-04-04 03:48:59 +0300

    Updates

---
+++ precache-manifest.cf8ef81ab99fe4a50da669e1c3383c5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27c8899819ceca3e2b1a79ca0aa44a4",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "57da87873771b6388bb2",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6210bf1ded25448980ff",
+    "url": "/react/static/js/2.2594f893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.2594f893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57da87873771b6388bb2",
+    "url": "/react/static/js/main.6ccd0ddd.chunk.js"
+  },
+  {
+    "revision": "dde54a01d2c790f3a35c",
+    "url": "/react/static/js/runtime-main.60163d52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
