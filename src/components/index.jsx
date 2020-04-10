45262f0b9b97e333c293d35fa7f97137c5efed13
privalenkov <privalenkovkirill@gmail.com>
2020-04-10 10:10:04 +0300

    final3

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,7 +7,7 @@ import Game from './pages/Game'
 import About from './pages/About'
 
 const routes = [
-  { path: '/react', name: 'WEB', Component: Web },
+  { path: '/', name: 'WEB', Component: Web },
   { path: '/Illustration', name: 'ILLUSTRATION', Component: Illustration },
   { path: '/Game', name: 'GAME', Component: Game },
   { path: '/About', name: 'ABOUT', Component: About },
