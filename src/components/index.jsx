1334240d61da4963516816b997711a7a0529c48f
privalenkov <privalenkovkirill@gmail.com>
2020-04-15 11:10:32 +0300

    hashrouter

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom'
 import '../style/index.css';
 import Web from './pages/Web'
 import Illustration from './pages/Illustration'
