9e6ab9d7773a65379a6339f21d3fbeec780068fc
privalenkov <privalenkovkirill@gmail.com>
2020-04-19 15:55:01 +0300

    'timeout'

--- src/components/Imgplate.jsx
+++ src/components/Imgplate.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import '../style/imgplate.css';
 import { Context } from '../context'
 
@@ -10,19 +9,12 @@ function Imgplate({ plate }) {
     ctx.setImages(plate.images)
   }
   return (
-    <TransitionGroup className="plate-container">
-      <CSSTransition
-        in={true}
-          appear={true}
-          timeout={100}
-          classNames="fade"
-      >
+    
         <div className="imga" >
           <button onClick={() => handleClick()}><div className="bg-plate"><div className="title-plate">{plate.title}</div></div></button>
           <img src= {plate.image} alt= {plate.title}/>
         </div>
-      </CSSTransition>
-    </TransitionGroup>
+    
   )
 }
 export default Imgplate
--- src/components/ImgplateList.jsx
+++ src/components/ImgplateList.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import Imgplate from './Imgplate'
 
 function ImgplateList({ imgplates }) {
+    let timeout = 0;
     const plateElements = imgplates.map((plate, index) =>
-        <Imgplate key= {index} plate = {plate}/>
+        <TransitionGroup className="plate-container">
+            <CSSTransition
+                in={true}
+                appear={true}
+                timeout={timeout += 100}
+                classNames="fade"
+            >
+                <Imgplate key= {index} plate = {plate}/>
+            </CSSTransition>
+        </TransitionGroup>
     )
     return (
         <>
+        
             {plateElements}
+            
         </>
     )
 }
